fd12ae8c86a6d9f3c50fb60c8c371a4082349467
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2023-04-08 10:24:40 +0800

    ci: drop support for Node.js 12

--- src/error.ts
+++ src/error.ts
@@ -2,8 +2,10 @@ export class JoseError extends Error {
   constructor(place: string, key: string, got?: string, expected?: string) {
     const message =
       expected === undefined
-        ? `${place} found "${key}" is not equal to ${got ?? 'unknown'}`
-        : `${place} "${key}" is got ${got ?? 'unknown'}, expected ${expected}`;
+        ? // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          `${place} found "${key}" is not equal to ${got}`
+        : // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          `${place} "${key}" is got ${got}, expected ${expected}`;
 
     super(message);
   }
